0f3595fe32da7565a1f16b1f05e95310fa7f2a11
yocto <yocto@androidbuilder>
2023-06-23 09:22:01 +0200

    Configurazione script di setup.

--- scripts/conf_egf.sh
+++ scripts/conf_egf.sh
@@ -2,7 +2,6 @@
 
 # GOOGLE git repo downloads 
 
-# GIT_REPO_DOWNLOADS_URL="https://storage.googleapis.com/git-repo-downloads/repo-1"
 
 GIT_REPO_DOWNLOADS_URL="http://commondatastorage.googleapis.com/git-repo-downloads/repo"
 # NXP repo configuration
@@ -12,29 +11,27 @@ VERSIONE="hardknott"
 BRANCH_NXP_VERSIONE="imx-linux-${VERSIONE}"
 MANIFEST_NXP="imx-5.10.35-2.0.0.xml"
 
-
-
 # EGF repo configuration
-
-PROGETTO="0800_egf_imx8_hardknott"
+PROGETTO="0700_vega_imx8_hardknott"
 EGF_MANIFEST_REPO="git@gitrepo.egf.it:egf/meta-egf-manifest.git"
-BRANCH_EGF_MANIFEST="0800_egf_imx8_hardknott"
+BRANCH_EGF_MANIFEST="700_vega_imx8_hardknott"
 MANIFEST_EGF="manifest.xml"
 
-# EGF FTP
-FTP_USER=""
-FTP_PASS=""
-FTP_URL="ftp://${FTP_USER}:${FTP_PASS}@ftp.elettronicagf.it:21/yocto/${VERSIONE}/egf_imx8_5.10.35_2.0.0/yocto-input.tar.gz"
+# EGF HTTP
+FTP_URL="http://@mantis.elettronicagf.it/Yocto/yocto/hardknott/egf_imx8_5.10.35_2.0.0/yocto-input.tar.gz"
+
 
 # DOCKER
 DOCKER_IMAGE="egf-image-${VERSIONE}-${PROGETTO}:latest"
 
+
+# FOLDERS BINDING
+
 MNT_DIR=$(pwd | xargs  dirname )
-PRG_DIR="/yocto2-nas/${VERSIONE}/${PROGETTO}"
 
 OUTPUT_SRC=${MNT_DIR}/yocto-output
-INPUT_SRC=${PRG_DIR}/yocto-input
-DOWNLOADS_SRC=${PRG_DIR}/downloads
+INPUT_SRC=${MNT_DIR}/yocto-input
+DOWNLOADS_SRC=${MNT_DIR}/downloads
 
 OUTPUT_DEST=/home/yocto/yocto-output
 INPUT_DEST=/home/yocto/yocto-input
